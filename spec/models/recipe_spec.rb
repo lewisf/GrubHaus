9f0f03522cdc14c13a28f72e015fd40b88a681ba
Lewis Chung <lewis.f.chung@gmail.com>
2012-10-07 16:43:02 -0700

    recipe specs git status

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 
 describe Recipe do
   
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:author) }
+  it { should validate_uniqueness_of(:name).scoped_to(:author) }
+
   before :each do
     @recipe = Recipe.new
   end
 
-  it "should be invalid without a title" do
-    expect(Recipe.save).to eq(false)
-  end
-
-  it "should be invalid without an owner" do
-    expect(Recipe.save).to eq(false)
-  end
-
   it "should be initialized in draft state" do
-    expect(Recipe.draft).to eq(true)
+    expect(@recipe.draft).to eq(true)
   end
+
+  it "should not allow draft to be true without all the fields filled in." do
+    @recipe.draft = false
+    expect(@recipe.save).to eq(false)
+  end
+
 
 
 end
