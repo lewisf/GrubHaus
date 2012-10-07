1512b089bb92a30ee0cce45871f343eb7b59ad09
Lewis Chung <lewis.f.chung@gmail.com>
2012-10-06 23:05:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrubHaus::Application.config.secret_token = '968f27fccc6be9dc07f7edbd918230a2cac4eb31fe2282df9d77321671f4c92fe9370569697571f6ff316141736879574fe17b138dc80bb4f16289f1ab2d8bc6'
