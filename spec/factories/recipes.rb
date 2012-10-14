ae31bf765dae3ef6ad945458ba7b4574d73b4798
Lewis Chung <lewis.f.chung@gmail.com>
2012-10-14 14:04:27 -0700

    more stuff in tests

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :recipe do
     name 'Spaghetti and Meatballs'
+    photo 'photo.png'
+    description 'A delicious, simple, budget Spaghetti and Meatballs recipe'
+    association :author, factory: :user, strategy: :build
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe Recipe do
   
+  it { should have_fields(:name, :author, :photo, :description) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:author) }
   it { should validate_uniqueness_of(:name).scoped_to(:author) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:photo) }
 
   before :each do
     @recipe = Recipe.new
@@ -14,11 +18,14 @@ describe Recipe do
     expect(@recipe.draft).to eq(true)
   end
 
-  it "should not allow draft to be true without all the fields filled in." do
+  it "should not allow draft to be true (should not publish) without all the fields filled in." do
     @recipe.draft = false
     expect(@recipe.save).to eq(false)
   end
 
-
+  it "should be able to find its parent recipe if it has one" do
+    @recipe.parent = Recipe.new
+    expect(@recipe.parent.class).to eq(Recipe)
+  end
 
 end
