5bfae1d77e0f3c0e56c3661462f502d6645f01a1
Lewis Chung <lewis.f.chung@gmail.com>
2012-10-23 10:16:00 -0700

    fixing typo

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,6 +6,6 @@ describe Ingredient do
   it { should have_many(:substitutes).of_type(Ingredient) }
 
   it { should validate_presence_of(:name) }
-  it { should validate_uniquenes_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 
 end
