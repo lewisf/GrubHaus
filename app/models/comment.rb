eab770458540c2e8e92223ecacb29cdab3443dbc
Lewis Chung <lewis.f.chung@gmail.com>
2012-10-25 12:58:37 -0700

    added functionality and tests for things to be commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment
   include Mongoid::Document
 
   field :content, type: String
-  embeds_many :comments
+  embeds_many :comments, as: :commentable
+  embedded_in :commentable, polymorphic: true
 
   validates_presence_of :content
 
