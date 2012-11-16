4d27c7262bd3cddee6a6f2e0c0cbb9b52efc16f9
Lewis Chung <lewis.f.chung@gmail.com>
2012-11-15 23:33:55 -0800

    added spaghetti and meatballs default recipe and styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,12 +87,53 @@ p "Created Gylmar"
 p "Creating recipes ..."
 Recipe.create do |recipe|
   recipe.name = "Spaghetti and Meatballs"
-  recipe.photo = "http://placekitten.com/190/190"
-  recipe.description = "Scrumptious fresh budget spaghetti and meatballs recipe"
-  recipe.prep_time = 30.to_s
-  recipe.cook_time = 30.to_s
-  recipe.ready_in = 60.to_s
-  recipe.serving_size = 4.to_s
+  recipe.photo = "http://mda.bigoven.com/pics/rs/256/cheesy-meatballs-with-spaghetti-2.jpg"
+  recipe.description = "For the best texture, don't overwork the meat mixture and use Parmesan that's ground to a fine powder 
+  (use the processor or the rasp side of a box grater). For more heat, add 1/2 to 3/4 teaspoon dried crushed red pepper to the sauce."
+  recipe.prep_time = 10.to_s
+  recipe.cook_time = 25.to_s
+  recipe.ready_in = 35.to_s
+  recipe.serving_size = 6.to_s
+  recipe.steps.new do |recipe|
+    recipe.description = "Preheat oven to 425 degrees Fahrenheit Place a large pot of water on the stove and heat until boiling, and cook pasta to al dente"
+    recipe.start_time = 0
+    recipe.end_time = 1
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Mix beef and Worcestershire, egg, bread crumbs, cheese, garlic, salt, and pepper. Roll meat into 1 1/2 inch medium-sized meatballs and place on nonstick cookie sheet or a cookie sheet greased with extra-virgin olive oil."
+    recipe.start_time = 1
+    recipe.end_time = 5
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Bake balls for 10 to 12 minutes, until no longer pink."
+    recipe.start_time = 5
+    recipe.end_time = 16
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Heat a deep skillet or medium pot over moderate heat. Add oil, crushed pepper, garlic, and finely chopped onion. Saute 5 to 7 minutes, until onion bits are soft."
+    recipe.start_time = 6
+    recipe.end_time = 13
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Add beef stock, crushed tomatoes and herbs. Bring to a simmer and cook for about 10 minutes."
+    recipe.start_time = 12
+    recipe.end_time = 22
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Take pasta off heat and drain using strainer."
+    recipe.start_time = 15
+    recipe.end_time = 16
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Toss hot, drained pasta in a few ladles of sauce and grated cheese. Turn meatballs in remaining sauce."
+    recipe.start_time = 22
+    recipe.end_time = 24
+  end
+  recipe.steps.new do |recipe|
+    recipe.description = "Serve!"
+    recipe.start_time = 25
+    recipe.end_time = 25
+  end
   recipe.author = @lewis
 end
 p "Created Spaghetti and meatballs"
